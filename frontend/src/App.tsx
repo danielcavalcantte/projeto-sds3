1f702efd362fc84fecaea8fe88e318e01ca1a805
Daniel Cavalcante <daniel16henrrique@gmail.com>
2021-09-07 23:29:34 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from 'components';
+import DataTable from 'components/Data Table';
+import Footer from 'components/Footer';
 import React from 'react';
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
         <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
